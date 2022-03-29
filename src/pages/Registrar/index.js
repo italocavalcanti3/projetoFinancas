575eb53ab86fb46c7207e20488ab6a2cec2a0920
Italo Cavalcanti <87244758+italocavalcanti3@users.noreply.github.com>
2022-03-28 22:53:46 -0300

    adicionando picker

--- src/pages/Registrar/index.js
+++ src/pages/Registrar/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from '../../components/Header';
+import Picker from '../../components/Picker/index.android';
+import { Background, Input, SubmitButton, SubmitText } from './style';
 
 export default function Registrar() {
+  const pickerRef = useRef(null);
+  const [valor, setValor] = useState('');
+  const [tipo, setTipo] = useState('receita');
+
   return (
-    <View>
-      <Header />
-    </View>
+    <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>
+      <Background>
+        <Header />
+
+        <SafeAreaView style={{ alignItems: 'center' }}>
+
+          <Input
+          placeholder='Valor desejado'
+          keyboardType='numeric'
+          returnKeyType='next'
+          ref={pickerRef}
+          value={valor}
+          onChangeText={ (valor) => setValor(valor) }
+          />
+
+          <Picker
+          onChange={setTipo}
+          tipo={tipo}
+          />
+
+          <SubmitButton>
+            <SubmitText>Registrar</SubmitText>
+          </SubmitButton>
+
+        </SafeAreaView>
+      </Background>
+    </TouchableWithoutFeedback>
   );
 }
